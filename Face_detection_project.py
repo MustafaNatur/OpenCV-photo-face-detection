57ed10684bb5685605ebeabcab128280791ecf47
MsustafaNatur <mustafa.natur@mail.ru>
2021-09-04 16:48:07 +0300

    Face_detection_project
    Face_detection_project

---
+++ Face_detection_project.py
@@ -0,0 +1,34 @@
+import cv2
+
+#The xml markup language is designed for convenient
+# encoding and reading of information in a
+# machine and manual way. The file structure
+# and its parameters are specified using tags,
+# attributes and preprocessors
+
+#open xml file
+faceCascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+#open img
+img = cv2.imread('Resources/1.jpg')
+
+#Set colour filter
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(imgGray,1.1,4)
+
+#set rectangle
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+    cv2.putText(img, "FACE ", (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 2)
+
+#show result
+cv2.imshow('Result', img)
+
+#delay
+cv2.waitKey(0)
+
+
+
+
+
